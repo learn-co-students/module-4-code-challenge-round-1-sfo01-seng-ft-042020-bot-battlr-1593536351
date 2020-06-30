bc1bd8fd26504ea9b2cc6ff505038ca54a6cced3
MagdalenaJasmine <magdalenacooper92@gmail.com>
2020-06-30 11:38:58 -0700

    Mod 4 Code Challenge

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import BotSpecs from "../components/BotSpecs";
+import BotCard from "../components/BotCard";
 
 class BotCollection extends Component {
   //your code here
@@ -7,8 +9,16 @@ class BotCollection extends Component {
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
-          Collection of all bots
+          {this.props.bots.map((bot) => (
+            <BotCard
+              key={bot.id}
+              bot={bot}
+              handleClick={this.props.addBotToArmy}
+            />
+          ))}
+          {/* bring back in if I get to advanced deliverables */}
+          {/* {this.props.bots.map((bot) => (
+            <BotSpecs key={bot.id} bot={bot} /> */}
         </div>
       </div>
     );
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BotCard from "../components/BotCard";
 
 class YourBotArmy extends Component {
   //your bot army code here...
@@ -8,7 +9,14 @@ class YourBotArmy extends Component {
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.props.bots.map((bot) => (
+              <BotCard
+                key={bot.id}
+                bot={bot}
+                handleClick={this.props.removeBot}
+                handleClick={this.props.dischargeBot}
+              />
+            ))}
             Your Bot Army
           </div>
         </div>
